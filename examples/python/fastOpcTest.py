b4f57c9a6cb90ea1fdcbcee31d2c166072534753
dgole <dgole100@gmail.com>
2017-11-07 11:21:52 -0700

    Update fastOpcTest.py

--- examples/python/fastOpcTest.py
+++ examples/python/fastOpcTest.py
@@ -8,9 +8,9 @@ import numpy as np
 numLEDs = 60
 client = fastopc.FastOPC('localhost:7890')
 
-pixels = np.zeros([3, numLEDs])
-pixels[0,10] = 100
-pixels[2,10] = 100
-pixels[1,12] = 200
+pixels = np.zeros(numLEDs)
+pixels[10] = 100
+pixels[11] = 100
+pixels[12] = 200
 
 client.putPixels(pixels)
