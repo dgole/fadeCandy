8226e54020f35d72ffcf3fd5411552215ca1e416
dgole <dgole100@gmail.com>
2017-11-07 11:09:50 -0700

    Update fastOpcTest.py

--- examples/python/fastOpcTest.py
+++ examples/python/fastOpcTest.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python
 
+# Light each LED in sequence, and repeat.
+
+import fastopc, time
+import numpy as np
+
+numLEDs = 60
+client = fastopc.FastOPC('localhost:7890')
+
+pixels = np.zeros([numLEDs,3])
+pixels[10,0] = 100
+pixels[10,2] = 100
+pixels[12,1] = 200
+
+client.put_pixels(pixels)
