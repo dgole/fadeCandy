0286a38d81f612797b82afe86c6e2a739c11bff9
dgole <dgole100@gmail.com>
2017-11-07 12:32:56 -0700

    Update fastOpcTest.py

--- examples/python/fastOpcTest.py
+++ examples/python/fastOpcTest.py
@@ -21,7 +21,8 @@ class Pixels():
 		alpha[alpha <= 0.0] = alphaDecay
 		self.array = alpha*arrayNew + (1.0-alpha)*self.array
 	def getArrayForDisplay(self):
-		return self.array[self.array < self.floor] = 0
+		returnArray = self.array[self.array < self.floor] = 0
+		return returnArray
 	
 
 n = 1
