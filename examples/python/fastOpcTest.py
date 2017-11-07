7649af43672a96787fa6ac4398b4ee1911f0941f
dgole <dgole100@gmail.com>
2017-11-07 12:03:07 -0700

    Update fastOpcTest.py

--- examples/python/fastOpcTest.py
+++ examples/python/fastOpcTest.py
@@ -10,16 +10,24 @@ client = fastopc.FastOPC('localhost:7890')
 
 pixels = np.zeros([numLEDs, 3])
 
+class Pixels(numLEDs):
+    def __init__(self, numLEDs):
+		self.numLEDs = numLEDs
+		self.array = np.zeros([self.numLEDs])
+    def set(array1, alpha):
+		self.array = alpha*array1 + (alpha-1.0)*array 
+
 n = 1
 dir = 1
-pixels[n, 2] = 255
+pixels = Pixels(numLEDs)
+pixels.array[n,2] = 255
 
 while True:
 	if n == 59:
 		dir*=-1
 	elif n == 0:
 		dir*=-1
-	pixels = np.roll(pixels, dir, axis=0)
-	client.putPixels(0, pixels)
+	pixels.set(np.roll(pixels.array, dir, axis=0), 0.1)
+	client.putPixels(0, pixels.array)
 	n+=dir
 	time.sleep(0.2)
